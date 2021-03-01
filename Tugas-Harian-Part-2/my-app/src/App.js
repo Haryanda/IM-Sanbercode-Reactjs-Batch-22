537c06421525bcccba2af80351796655175d4d67
haryanda <harryharyanda@gmail.com>
2021-03-02 01:32:32 +0700

    tugas

--- Tugas-Harian-Part-2/my-app/src/App.js
+++ Tugas-Harian-Part-2/my-app/src/App.js
@@ -1,38 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tugas9 from './Tugas9/FormBuah'
+import Tugas10 from './Tugas10/DaftarBuah'
+import Tugas11 from './Tugas11/Jam'
+
+import './App.css'
 
 function App() {
   return (
-    <div>
-    <center>
-      <h1>Form Pembelian Buah</h1>
-    </center>
-    <form className="form-wrapper">
-      <table>
-        <tr>
-          <td width="200">Nama Pelanggan</td>
-          <td>
-            <input type="text" name="nama" />
-          </td>
-        </tr>
-        <tr>
-          <td>Daftar Item</td>
-          <td>
-            <input type="checkbox" name="buah" value="0" /> Semangka<br/>
-            <input type="checkbox" name="buah" value="1" /> Jeruk<br/>
-            <input type="checkbox" name="buah" value="2" /> Nanas<br/>
-            <input type="checkbox" name="buah" value="3" /> Salak<br/>
-            <input type="checkbox" name="buah" value="4" /> Anggur<br/>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            <button type="submit" value="Submit">Kirim</button>
-          </td>
-        </tr>
-      </table>
-    </form>
-    </div>
+    <>
+      <Tugas9/>
+      <Tugas10/>
+      <Tugas11/>
+    </>
   );
 }
 
---
+++ Tugas-Harian-Part-2/my-app/src/Tugas11/Jam.js
@@ -0,0 +1,61 @@
+import React, { Component, Fragment } from 'react';
+
+class Jam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      waktu: '',
+      timer: 10,
+    };
+  }
+
+  tick() {
+    const date = new Date();
+
+    const waktu = date.toLocaleString('id-ID', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    });
+
+    const timer = this.state.timer - 1;
+
+    this.setState({
+      waktu,
+      timer,
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    const row = {
+      display: 'flex',
+      alignItems: 'center',
+    };
+
+    const col = {
+      width: '50%',
+    };
+
+    return (
+      <Fragment>
+        {this.state.timer > 0 && (
+          <div style={row}>
+            <div style={col}>Sekarang Jam: {this.state.waktu}</div>
+            <div style={col}>Hitung Mundur: {this.state.timer}</div>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+}
+
+export default Jam;

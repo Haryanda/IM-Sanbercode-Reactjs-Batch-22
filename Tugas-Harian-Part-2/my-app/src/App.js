fc2edb23cd8342357f57fed92fc2af89c4688b3e
haryanda <harryharyanda@gmail.com>
2021-03-04 05:51:27 +0700

    Tugas13

--- Tugas-Harian-Part-2/my-app/src/App.js
+++ Tugas-Harian-Part-2/my-app/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Tugas9 from './Tugas9/FormBuah'
-import Tugas10 from './Tugas10/DaftarBuah'
-import Tugas11 from './Tugas11/Jam'
-import Tugas12 from './Tugas12/DaftarBuah'
+//import Tugas9 from './Tugas9/FormBuah'
+//import Tugas10 from './Tugas10/DaftarBuah'
+//import Tugas11 from './Tugas11/Jam'
+//import Tugas12 from './Tugas12/DaftarBuah'
+import Tugas13 from './Tugas12/DaftarBuah'
+
 
 import './App.css'
 
 function App() {
   return (
     <>
-      <Tugas9/>
-      <Tugas10/>
-      <Tugas11/>
-      <Tugas12/>
+      <Tugas13/>
     </>
   );
 }
